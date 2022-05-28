390dd1f64dfeb8c3ac4bde6deb0677ccb1965510
Loc DEV <locdev2000@gmail.com>
2022-05-28 20:38:05 +0700

    deploy client

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,11 +1,11 @@
 // const api =
 //   process.env.NODE_ENV !== "production"
 //     ? "http://localhost:5000"
-//     : "https://realestate-app-be.herokuapp.com";
+//     : "https://realestate-be-app.herokuapp.com";
 
 const api =
   process.env.NODE_ENV !== "production"
-    ? "https://realestate-app-be.herokuapp.com"
+    ? "https://realestate-be-app.herokuapp.com"
     : "http://localhost:5000";
 
 export default api;
